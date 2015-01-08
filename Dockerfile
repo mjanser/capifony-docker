5e8df5b45332ed8f2b34f31003bc490300e3b12f
Martin Janser <martin@duss-janser.ch>
2015-01-08 11:36:50 +0100

    Update to 2.8.3 and set SELinux context on files if enabled

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM debian:jessie
 MAINTAINER Martin Janser <martin@duss-janser.ch>
 
 RUN apt-get update && apt-get install -y rubygems git
-RUN gem install capifony -v 2.8.2
+RUN gem install capifony -v 2.8.3
 
 ENV SSH_AUTH_SOCK /root/ssh-agent
 
--- cap.sh
+++ cap.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+WORKDIR=$(pwd)
+SSH_AGENT=$(readlink -f $SSH_AUTH_SOCK)
+
+if [ $(command -v selinuxenabled >/dev/null 2>&1) -a $(selinuxenabled) ]; then
+    chcon -Rt svirt_sandbox_file_t $WORKDIR $SSH_AGENT
+fi
+
 docker run \
     --rm \
     -i \
     -t \
-    -v $(pwd):/root/workdir \
-    -v $(readlink -f $SSH_AUTH_SOCK):/root/ssh-agent \
+    -v $WORKDIR:/root/workdir \
+    -v $SSH_AGENT:/root/ssh-agent \
     mjanser/capifony \
     $@
